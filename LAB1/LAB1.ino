f74bc30e01bc3a86e950270a04506686cd8acf09
Dwyn Jiro C. Lising <lising.dwynjiro.c@gmail.com>
2026-08-11 11:34:10 +0800

    Update comment in LAB1.ino

--- LAB1/LAB1.ino
+++ LAB1/LAB1.ino
@@ -1,4 +1,4 @@
-#include "_sevenSegment.h" // Sheena entered this world; pitipiwpiw wiwiw
+#include "_sevenSegment.h" // Sheena entered this world; pitipiwpiw wiwiw; ay balew
 
 const byte segPins[7] = {13, 12, 11, 10, 9, 8, 7};
 const byte BT1 = 6, BT2 = 5, BT3 = 4, BT4 = 3, BT5 = 2;
